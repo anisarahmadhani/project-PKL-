2c493f1e714977aba8032d154f84950c7811fab8
anisarahmadhani <120235072+anisarahmadhani@users.noreply.github.com>
2022-12-11 22:25:04 +0700

    Add files via upload

---
+++ database/migrations/2022_12_05_213645_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->foreignId('client_id');
+            $table->date('project_start');
+            $table->date('project_end');
+            $table->char('project_status',15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
